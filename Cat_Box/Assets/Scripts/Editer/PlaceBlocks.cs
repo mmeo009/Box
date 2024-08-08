c7f7f11a6d3b08f78b7edf513f8571f90d1cc0f4
mmeo009 <shh020615@gmail.com>
2024-08-08 18:00:55 +0900

    [Feat] 프리팹 불러오기, 생성만 안된당 ㅠㅠ

--- Cat_Box/Assets/Scripts/Editer/PlaceBlocks.cs
+++ Cat_Box/Assets/Scripts/Editer/PlaceBlocks.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using System;
+using static UnityEditor.PlayerSettings;
 
 public class PlaceBlocks : EditorWindow
 {
-    public List<GameObject> prefabs = new List<GameObject>();
+    public static Dictionary<string, GameObject> prefabs = new Dictionary<string, GameObject>();
 
-    public Vector3 positionToPlace = Vector3.zero;
+    public Vector3Int positionToPlace = Vector3Int.zero;
 
-    public int repetition_X = 0;
-    public int repetition_Y = 0;
+    public bool isFill;
+
+    public Vector3Int from;
+    public Vector3Int to;
+
+    public string selectedPrefabName;
 
 
     [MenuItem("CatBox/Prop Generator")]
@@ -23,15 +29,122 @@ public class PlaceBlocks : EditorWindow
         window.Show();
     }
 
-    private void OnGUI()
+    private string GetAssetName(string path)
     {
-        prefabs.Add(AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath("Prefabs/Map")));
+        string assetName = path.Substring(path.LastIndexOf('/') + 1);
+        return assetName;
+    }
+    private void LoadPrefabsDatas()
+    {
+        string[] AssetGuids = AssetDatabase.FindAssets("t:prefab", new[] { "Assets/Prefabs/Map" });
+        string[] AssetPathList = Array.ConvertAll<string, string>(AssetGuids, AssetDatabase.GUIDToAssetPath);
 
-        for (int i = 0; i < prefabs.Count; i ++)
+
+        foreach (string path in AssetPathList)
         {
-            
+            var prefabName = GetAssetName(path);
+
+            if (!prefabs.ContainsKey(prefabName))
+            {
+                var prefab = AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)) as GameObject;
+                prefabs.Add(prefabName, (GameObject)prefab);
+                Debug.Log(prefabName + "을 불러왔습니다.");
+            }
+        }
+    }
+    private void SelectPrefab(string prefabName)
+    {
+        selectedPrefabName = prefabName;
+    }
+    private void CreatePrefab()
+    {
+        if(isFill)
+        {
+            for(int i = from.x; i <= to.x; i++) 
+            {
+                if(from.y != 0 && to.y != 0 && from.y != to.y)
+                {
+                    for (int j = from.y; j <= to.y; j++)
+                    {
+                        for (int k = from.z; k <= to.z; k++)
+                        {
+                            var position = new Vector3(i, j, k);
+                            var temp = Instantiate(prefabs[selectedPrefabName], position, Quaternion.identity);
+                            temp.transform.position = new Vector3(i, j, k);
+                        }
+
+                    }
+                }
+                for (int k = from.z; k <= to.z; k++)
+                {
+                    var position = new Vector3(i, 0, k);
+                    var temp = Instantiate(prefabs[selectedPrefabName], position, Quaternion.identity);
+                    temp.transform.position = new Vector3(i, 0, k);
+                }
+            }
+        }
+        else
+        {
+            var temp = Instantiate(prefabs[selectedPrefabName]);
+            temp.transform.position = positionToPlace;
         }
 
+        positionToPlace = Vector3Int.zero;
+        from = Vector3Int.zero;
+        to = Vector3Int.zero;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+
+        if(GUILayout.Button("프리팹 불러오기"))
+        {
+            LoadPrefabsDatas();
+        }
+
+        if(GUILayout.Button("생성하기"))
+        {
+            CreatePrefab();
+        }
+
+        GUILayout.EndHorizontal();
+        GUILayout.Space(10);
+
+        GUILayout.BeginVertical();
+
+        GUILayout.BeginHorizontal();
+
+        positionToPlace = EditorGUILayout.Vector3IntField("설치할 곳", positionToPlace);
+
+        if (selectedPrefabName != null)
+        {
+            GUILayout.Label("선택된 오브젝트 :" +  selectedPrefabName);
+        }
+
+        GUILayout.EndHorizontal();
+
+        isFill = EditorGUILayout.BeginToggleGroup("반복", isFill);
+        if (isFill)
+        {
+            from = EditorGUILayout.Vector3IntField("시작지점", from);
+            to = EditorGUILayout.Vector3IntField("끝나는 지점", to);
+        }
+
+        EditorGUILayout.EndToggleGroup();
+        GUILayout.EndVertical();
+
+        GUILayout.Space(10);
+
+        GUILayout.BeginVertical();
+
+        foreach(var prefab in prefabs)
+        {
+            if(GUILayout.Button(prefab.Key))
+            {
+                SelectPrefab(prefab.Key);
+            }
+        }
     }
 
 }
